8ccf4c90c585e0704a690e482a33074aaabd1494
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-14 10:58:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,7 @@ class Application
     #   item_name = req.path.split("/items/").last
     #   item = @@items.find{|item| item.name == item_name}
     #   resp.write item.price
-    # binding.pry
     if req.path=="/items/Figs"
-      # binding.pry
       resp.write "#{@@items[0].price}"
     else
       resp.write "Route not found"
